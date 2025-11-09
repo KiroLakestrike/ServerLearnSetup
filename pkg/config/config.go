3ea3f739a2353043d0f112c462f5fabc870c014d
Pascal Klodel <99654847+KiroLakestrike@users.noreply.github.com>
2025-11-09 17:39:34 +0100

    Introduce session management and middleware updates
    - Add session management using `scs` package.
    - Extend `AppConfig` with session-related fields.
    - Implement session middleware for request handling.
    - Update templates to display user's remote IP address stored in session.
    - Enhance CSRF protection to respect production settings.

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,10 +1,18 @@
 package config
 
-import "html/template"
+import (
+	"html/template"
+	"log"
+
+	"github.com/alexedwards/scs/v2"
+)
 
 // This is just a basic configuation file
 type AppConfig struct {
 	UseCache      bool
 	TemplateCache map[string]*template.Template
+	InfoLog       *log.Logger
 	PortNumber    string
+	InProduction  bool
+	Session       *scs.SessionManager
 }
